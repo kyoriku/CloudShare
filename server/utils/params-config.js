537b0e3b60461b2357001c48b084e8cdf436c7bb
Austin Graham <austiingraham@gmail.com>
2024-09-16 19:51:44 -0400

    completed image upload integration

---
+++ server/utils/params-config.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+
+const { v4: uuidv4 } = require('uuid');
+
+const params = (fileName) => {
+  const myFile = fileName.originalname.split('.');
+  const fileType = myFile[myFile.length - 1];
+
+  const imageParams = {
+    Bucket: process.env.AWS_BUCKET_NAME,
+    Key: `${uuidv4()}.${fileType}`,
+    Body: fileName.buffer,
+  };
+
+  return imageParams;
+};
+
+module.exports = params;
